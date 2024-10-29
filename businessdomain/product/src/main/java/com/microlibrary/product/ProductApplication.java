01456194eab017969f20363f91c6793d28887f83
Sergio <sergio160502@gmail.com>
2024-10-29 15:10:50 +0100

    Actualizacion

--- businessdomain/product/src/main/java/com/microlibrary/product/ProductApplication.java
+++ businessdomain/product/src/main/java/com/microlibrary/product/ProductApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductApplication {
+    
+    //hecho jenkins
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
