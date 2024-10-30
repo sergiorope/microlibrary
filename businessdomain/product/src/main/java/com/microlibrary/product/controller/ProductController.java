312836eae9cc963bc56cbf31d3391cd3a4a09547
Sergio <sergio160502@gmail.com>
2024-10-31 00:02:43 +0100

    integracion

--- businessdomain/product/src/main/java/com/microlibrary/product/controller/ProductController.java
+++ businessdomain/product/src/main/java/com/microlibrary/product/controller/ProductController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Tag(name = "Product API", description = "This APi serve all functionality for management Products")
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/product")
 public class ProductController {
 
---
+++ front/js/productos.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const apiUrl = 'http://localhost:8080/product/list'; 
+
+    
+    fetch(apiUrl)
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error('Error en la red');
+            }
+            return response.json(); 
+        })
+        .then(data => {
+          
+            console.log(data);
+            document.getElementById('result').textContent = JSON.stringify(data, null, 2); 
+        })
+        .catch(error => {
+        
+            console.error('Hubo un problema con la petición Fetch:', error);
+            document.getElementById('result').textContent = 'Error: ' + error.message;
+        });
+});
