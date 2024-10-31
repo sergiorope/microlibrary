bf4244df6fac62f03398f7ceab95125030d8f134
Sergio <sergio160502@gmail.com>
2024-10-31 17:08:24 +0100

    Committing local changes

--- front/js/productos.js
+++ front/js/productos.js
@@ -10,7 +10,34 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             console.log(data);
-            document.getElementById('result').textContent = JSON.stringify(data, null, 2); 
+
+            const resultContainer = document.getElementById('result');
+            resultContainer.innerHTML = '';
+
+            
+            data.forEach(item => {
+                
+                const nameElement = document.createElement('span');
+                nameElement.className = 'product-name';
+                nameElement.textContent = item.name; 
+
+                const descriptionElement = document.createElement('span');
+                descriptionElement.className = 'product-description';
+                descriptionElement.textContent = item.description;
+
+                const imageElement = document.createElement('img');
+                imageElement.className = 'product-image';
+                imageElement.src = item.image;
+           
+
+                resultContainer.appendChild(nameElement);
+                resultContainer.appendChild(document.createElement('br')); 
+                resultContainer.appendChild(descriptionElement);
+                resultContainer.appendChild(document.createElement('br')); 
+                resultContainer.appendChild(imageElement);
+                resultContainer.appendChild(document.createElement('br')); 
+                
+            });
         })
         .catch(error => {
             console.error('Hubo un problema con la petición Fetch:', error);
