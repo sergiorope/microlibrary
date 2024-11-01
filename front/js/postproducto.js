15355923bc6b26974018b1d5cb55566c3b8ebad8
Sergio <sergio160502@gmail.com>
2024-11-01 18:32:43 +0100

    Committing local changes

--- front/js/postproducto.js
+++ front/js/postproducto.js
@@ -7,24 +7,26 @@ form.addEventListener("submit", (event) => {
     const data = {};
     formData.forEach((value, key) => (data[key] = value));
 
-
-fetch("http://localhost:8080/product/post", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json; charset=UTF-8"
-    },
-    body: JSON.stringify(data) 
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error("Error en la red");
-    }
-    return response.json();
-})
-.then(data => {
-    console.log("Producto guardado exitosamente:", data);
-})
-.catch(error => {
-    console.error("Hubo un problema con la petición Fetch:", error);
-});
+  
+    fetch("http://localhost:8080/product/post", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(data) 
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.json().then(err => { // Obtener el mensaje de error del servidor
+                throw new Error(`Error ${response.status}: ${err.message}`);
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Producto guardado exitosamente:", data);
+    })
+    .catch(error => {
+        console.error("Hubo un problema con la petición Fetch:", error);
+    });
 });
