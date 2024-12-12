3173e29458af4c1413f8c48953f25b3026ce0e1e
Sergio <sergio160502@gmail.com>
2024-12-12 22:31:30 +0100

    Nuevos archivos para despliegue en un clúster de Kubernetes

--- businessdomain/product/src/main/java/com/microlibrary/product/ProductApplication.java
+++ businessdomain/product/src/main/java/com/microlibrary/product/ProductApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductApplication {
     
-    //hecho jenkins
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
